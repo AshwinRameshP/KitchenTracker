e3f07f01ba5a263da5fdb3179b634944065ecaa0
ashwin <ashwinrameshp@gmail.com>
2022-04-25 20:54:43 +0530

    Updated shopping list,edit and recipies

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+ 
+  recipes: Recipe[] = [
+  new Recipe('A test 1 recipe','This is a simple test', 'http://picsum.photos/200'),
+  new Recipe('A test 2 recipe','This is a simple test', 'http://picsum.photos/200'),
+  new Recipe('A test 3 recipe','This is a simple test', 'http://picsum.photos/200')
+  ];
 
   constructor() { }
 
